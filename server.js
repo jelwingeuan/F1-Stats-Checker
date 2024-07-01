f96c8bd2c87a772324a024ad9d768ddd0b998981
jelwingeuan <jelwingeuanjw@gmail.com>
2024-07-01 12:12:41 +0800

    local host

--- server.js
+++ server.js
@@ -6,3 +6,23 @@ const app = express();
 const PORT = 5000;
 
 const API_URL = "https://ergast.com/api/f1";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
