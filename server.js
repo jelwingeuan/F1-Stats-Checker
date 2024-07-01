e0c4cf467ae584e6e79f0487d36949ff11912df8
jelwingeuan <jelwingeuanjw@gmail.com>
2024-07-01 12:14:08 +0800

    drivers&races data

--- server.js
+++ server.js
@@ -7,21 +7,23 @@ const PORT = 5000;
 
 const API_URL = "https://ergast.com/api/f1";
 
+app.get("/api/drivers", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/current/driverStandings.json`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/api/races", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/current.json`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
