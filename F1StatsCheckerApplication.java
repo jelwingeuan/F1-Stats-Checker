f87b11c88e59cf25876361f9b9813cfbd7de9737
jelwingeuan <jelwingeuanjw@gmail.com>
2024-07-20 14:37:35 +0800

    f1statscheckerspp

---
+++ F1StatsCheckerApplication.java
@@ -0,0 +1,11 @@
+package com.example.f1statschecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1StatsCheckerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(F1StatsCheckerApplication.class, args);
+    }
+}
