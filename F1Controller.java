87241b804af7c5350c78ab83a68dfa8e9f19f061
jelwingeuan <jelwingeuanjw@gmail.com>
2024-07-30 22:13:39 +0800

    controller sep

--- F1Controller.java
+++ F1Controller.java
@@ -1,55 +1,31 @@
 package com.example.f1statschecker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class F1Controller {
 
+    private final F1Service f1Service;
+
+    @Autowired
+    public F1Controller(F1Service f1Service) {
+        this.f1Service = f1Service;
+    }
+
     @GetMapping("/drivers")
-    public List<Driver> getDrivers() {
-        return Arrays.asList(
-                new Driver("Lewis Hamilton", "Mercedes", 7),
-                new Driver("Max Verstappen", "Red Bull Racing", 2),
-                new Driver("Sebastian Vettel", "Retired", 4)
-        );
+    public ResponseEntity<List<Driver>> getDrivers() {
+        List<Driver> drivers = f1Service.getDrivers();
+        return ResponseEntity.ok(drivers);
     }
 
     @GetMapping("/teams")
-    public List<Team> getTeams() {
-        return Arrays.asList(
-                new Team("Mercedes", 8),
-                new Team("Red Bull Racing", 5),
-                new Team("Ferrari", 16)
-        );
-    }
-
-    static class Driver {
-        private String name;
-        private String team;
-        private int championships;
-
-        public Driver(String name, String team, int championships) {
-            this.name = name;
-            this.team = team;
-            this.championships = championships;
-        }
-
-        // Getters and setters
-    }
-
-    static class Team {
-        private String name;
-        private int championships;
-
-        public Team(String name, int championships) {
-            this.name = name;
-            this.championships = championships;
-        }
-
-        // Getters and setters
+    public ResponseEntity<List<Team>> getTeams() {
+        List<Team> teams = f1Service.getTeams();
+        return ResponseEntity.ok(teams);
     }
 }
