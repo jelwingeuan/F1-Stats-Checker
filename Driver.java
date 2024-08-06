e854a22452d7743ffe39da58183ebf28a4b1f9e2
jelwingeuan <jelwingeuanjw@gmail.com>
2024-08-07 01:15:23 +0800

    driver

--- Driver.java
+++ Driver.java
@@ -4,11 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+import java.util.Optional;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Driver {
+public class Driver implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String name;
     private String team;
     private int championships;
+
+    public Optional<String> getName() {
+        return Optional.ofNullable(name);
+    }
+
+    public Optional<String> getTeam() {
+        return Optional.ofNullable(team);
+    }
+
+    public int getChampionships() {
+        return championships;
+    }
 }
